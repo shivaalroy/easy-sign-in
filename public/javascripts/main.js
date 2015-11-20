aef5314bab612492fad0f1a48512bb8cfe6e4317
Shivaal Roy <shivaal@stanford.edu>
2015-11-20 04:18:50 -0800

    sign in working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,25 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('SUID:' + profile.getEmail());
+  if (profile) {
+	  $("#loading-text").hide();
+	  $("#not-signed-in").hide();
+	  $("#signed-in .header").text("Signed in as " + profile.getEmail());
+	  $("#signed-in").show();
+  }
 }
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
+  auth2.signOut().then(function() {
+    $("#loading-text").hide();
+  	$("#not-signed-in").show();
+  	$("#signed-in").hide();
   });
 }
 
+function onFailure(error) {
+  console.log(error);
+}
+
 $(function() {
-	console.log("hello there");
 });
